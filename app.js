b778b9f71e2c67dc141c8b8ea4551dcb4e7f8ebb
Kevin O'Brien <kob123@hotmail.co.uk>
2018-11-13 11:55:08 +0000

    Added first event listeners

--- app.js
+++ app.js
@@ -1,50 +1,47 @@
-// Example of a 'module' in JS. An IIFE that returns an object with a method.
-//
-// budgetController.add(5) doesn't work because it's effectively a private
-// method (because budgetController is an IIFE).
-//
-// budgetController.publicTest(5) works because budgetController is an IIFE
-// that returns an object, of which publicTest is a method. So when the JS
-// runtime runs budgetController becomes the object the IIFE returns and the
-// publicTest method can be run on it.
-//
-// The publicTest method will always have access to variable x and function add()
-// because of closure.
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
-    var x = 23;
-
-    var add = function(a) {
-      return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-          return add(b);
-        }
-    }
+    // some code
 
 })();
 
+
+// UI CONTROLLER
 var UIController = (function() {
 
     // some code
 
 })();
 
-// Best practice to pass budgetController and UIController into controller
-// rather than just access them in the global scope. Why? If the names of these
-// object.methods changes, you only need to change the name of the argument passed
-// into controller rather than the (potentially multiple) uses of it in the
-// controller object.
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(30);
+  var ctrlAddItem = function() {
 
-    return {
-      anotherPublicMethod: function() {
-        console.log(z);
-      }
+    // 1. Get the field input data
+
+    // 2. Add the item to the bidget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
+    console.log('It works');
+  }
+
+  document.querySelector(".add__btn").addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+
+    // If the enter key is pressed
+    if (event.keycode === 13 || event.which === 13) {
+      ctrlAddItem();
     }
+
+
+  });
 
 })(budgetController, UIController);
