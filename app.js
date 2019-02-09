594725df633200ad509d00c9f13038f762731d7c
Kevin O'Brien <kob123@hotmail.co.uk>
2019-02-09 17:28:09 +0000

    Storing an entered budget item in the data object

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
-    // A function constructor. We use this because there will be lots of expenses.
-    // Creating a new Expense object example: var exp = new Expense(1, test, 100);
+    // A function constructor that creates an object. We use this because there 
+    // will be lots of expenses.Creating a new Expense object example: 
+    // var exp = new Expense(1, test, 100);
     var Expense = function(id, description, value) {
       this.id = id;
       this.description = description;
@@ -23,6 +24,43 @@ var budgetController = (function() {
       totals: {
         exp: 0,
         inc: 0
+      }
+    }
+
+    // The budgetController function returns this object when the app 
+    // loads/reloads because its an IIFE.
+    return {
+      addItem: function(type, des, val) {
+        var newItem, ID;
+
+        // Create new ID
+        // Can use bracket object access notation because the argument 
+        // type uses the same term as the items object property.
+        // The 'inc' and 'exp' arrays hold Expense and Income objects, respectively.
+        // So here we're accessing the id property of the last object in the array
+        // and adding 1 to create the ID of the next object to be added to the array.
+        if (data.items[type].length > 0) {
+          ID = data.items[type][data.items[type].length - 1].id + 1;
+        } else {
+          ID = 0;
+        }
+        
+
+        // Create new item based on 'inc' or 'exp' type
+        if (type === 'exp') {
+          newItem = new Expense(ID, des, val);
+        } else if (type === 'inc') {
+          newItem = new Income(ID, des, val);
+        }
+        // Push it into our data structure
+        data.items[type].push(newItem);
+
+        // Enables access to the newItem using budgetController.addItem()
+        return newItem;
+      },
+
+      testing: function() {
+        console.log(data);
       }
     }
 
@@ -70,11 +108,13 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function() {
+    var input, newItem;
 
     // 1. Get the field input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
 
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add the item to the UI
 
@@ -93,3 +133,15 @@ var controller = (function(budgetCtrl, UICtrl) {
 })(budgetController, UIController);
 
 controller.init();
+
+/* The controller function is called automatically when the app loads/reloads,
+   and the budgetController and UIController functions are passed in, allowing 
+   the controller function to access both their functions and objects.
+   What's the difference between budgetController and UIController just containing
+   an object and returning an object? It seems both can be accessed in the same way
+   e.g. budgetController.data.items.exp and budgetController.addItem
+
+   Because budgetController.data.items.exp won't work. budgetController returns an 
+   object that doesn't have data as a property! The only way to access the data object 
+   is using a getter function such as getDOMSelectors() in UIController.
+*/
